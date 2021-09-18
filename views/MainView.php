0b60f866926ed5538ba8454a73bf3ecaf837d932
mbence01 <h048514@stud.u-szeged.hu>
2021-09-18 15:27:44 +0200

    added MainView

--- views/MainView.php
+++ views/MainView.php
@@ -1,8 +1,19 @@
 <html>
     <head>
         <title>Main page</title>
+
+        <link rel="stylesheet" href="/css/style.css">
     </head>
     <body>
-        <h2>Hello world</h2>
+        <div id="main-container">
+            <p id="main-welcome-text">Welcome to the main page!</p>
+
+            <p id="main-welcome-subtext">Please choose a menu listed below</p>
+
+            <div id="main-buttons">
+                <a href="/List/Users" class="main-btn">Registered users</a>
+                <a href="/List/Ads" class="main-btn">Advertisements</a>
+            </div>
+        </div>
     </body>
 </html> 
